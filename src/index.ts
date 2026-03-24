38e43cb4df2405793d28713d18ef83a095854f5b
Akshat Prakash Kanth <110756105+Akshatkanth@users.noreply.github.com>
2026-03-24 20:41:36 +0530

    feat: add initial server setup with Express, dotenv, and PostgreSQL/Redis configuration

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, {Application, Request, Response} from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Application = express()
+const PORT = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+
+//health check
+app.get('/health', (req: Request, res:Response)=>{
+    res.json({status:"ok"})
+})
+
+//start server
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
