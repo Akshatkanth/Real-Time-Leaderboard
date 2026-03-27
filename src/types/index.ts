19b48993bafffc273d465d7533e438bfd802409f
Akshat Prakash Kanth <110756105+Akshatkanth@users.noreply.github.com>
2026-03-27 22:10:51 +0530

    feat: define RegisterInput, LoginInput, and JwtPayload interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface RegisterInput {
+  username: string
+  email: string
+  password: string
+}
+
+export interface LoginInput {
+  email: string
+  password: string
+}
+
+export interface JwtPayload {
+  userId: string
+  email: string
+}
