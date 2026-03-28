3de9f0e30062f3b6b264895580f183392effb820
Akshat Prakash Kanth <110756105+Akshatkanth@users.noreply.github.com>
2026-03-28 22:47:26 +0530

    feat: implement register and login controller functions

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,3 +2,36 @@
 // send back the response. 
 // It should have no business logic inside it. Just receive, delegate, respond.
 
+import {Request, Response} from 'express'
+import { registerUser, loginUser } from '../services/auth.service'
+import { RegisterInput, LoginInput } from '../types'
+
+export const register = async (req:Request, res:Response) => {
+    try{
+        const input: RegisterInput = req.body
+        const user = await registerUser(input)
+        res.status(201).json({
+            message:'User registered successfully',
+            data: user
+        })
+    }catch(error:any){
+        res.status(400).json({
+            message:error.message
+        })
+    }
+}
+
+export const login = async(req:Request, res:Response) => {
+    try {
+        const input: LoginInput = req.body;
+        const result = await loginUser(input)
+        res.status(200).json({
+            message:'Login successful',
+            data:result
+        })
+    } catch (error:any) {
+        res.status(400).json({
+            message:error.message
+        })
+    }
+}
